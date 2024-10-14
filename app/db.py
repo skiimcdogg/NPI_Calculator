70e573ee7b682fc3f2e95dfd9962a05052e36ff1
Antoine Stouff <antoine-78280@hotmail.fr>
2024-10-14 20:09:31 +0200

    feat/add env and docker

--- app/db.py
+++ app/db.py
@@ -1,12 +1,14 @@
 from motor.motor_asyncio import AsyncIOMotorClient
+from dotenv import load_dotenv
 import os
 
-# MONGO_USER = os.getenv("MONGO_I
-MONGO_INITDB_ROOT_USERNAME = "root"
-MONGO_INITDB_ROOT_PASSWORD = "root" # [ ] envs
-MONGO_HOST = "localhost"
-MONGO_PORT = "27017"
-MONGO_DB_NAME = "calculations"
+load_dotenv()
+
+MONGO_INITDB_ROOT_USERNAME = os.getenv("MONGO_INITDB_ROOT_USERNAME")
+MONGO_INITDB_ROOT_PASSWORD = os.getenv("MONGO_INITDB_ROOT_PASSWORD")
+MONGO_HOST = os.getenv("MONGO_HOST")
+MONGO_PORT = os.getenv("MONGO_PORT")
+MONGO_DB_NAME = os.getenv("MONGO_DB_NAME")
 
 MONGO_DETAILS = f"mongodb://{MONGO_INITDB_ROOT_USERNAME}:{MONGO_INITDB_ROOT_PASSWORD}@{MONGO_HOST}:{MONGO_PORT}/{MONGO_DB_NAME}?authSource=admin"
 print(MONGO_DETAILS)
@@ -21,5 +23,4 @@ async def create_collection_if_not_exists():
         await database.create_collection(collection_name)
         print(f"Collection '{collection_name}' created in DB '{MONGO_DB_NAME}'.")
 
-# Collection des calculs
 calculations_collection = database.get_collection("calculations")
